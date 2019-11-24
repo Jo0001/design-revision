5841db688abc7d905ed9adc745117b3bc83a9200
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2019-11-24 19:15:54 +0100

    added hash generation

---
+++ reset.php
@@ -0,0 +1,26 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=design_revision', 'dsnRev', '4_DiDsrev2019');
+
+if (!empty($_POST['email'])) {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+
+    $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+    $result = $statement->execute(array('email' => $email));
+    $user = $statement->fetch();
+
+    if ($user !== false) {
+        generateHash($pdo);
+
+    }
+}
+
+//TODO Needs testing with more users
+function generateHash($pdo){
+    do{
+       $hash = bin2hex(openssl_random_pseudo_bytes(128));
+        $statement = $pdo->prepare("SELECT * FROM users WHERE reset_id = :reset_id");
+        $result = $statement->execute(array('reset_id' => $hash));
+        $token = $statement->fetch();
+    }while($token !== false);
+    return $hash;
+}
