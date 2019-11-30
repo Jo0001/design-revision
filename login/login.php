55a7f0cbddf666ffd7fc04274ca0ea88dc5f9dc9
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2019-11-30 20:13:58 +0100

    improved login

--- login/login.php
+++ login/login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $pdo = new PDO('mysql:host=localhost;dbname=design_revision', 'dsnRev', '4_DiDsrev2019');
-
+//TODO check if user is logged in
 if (!empty($_POST['password']) && !empty($_POST['email'])) {
     $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
     $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
@@ -11,7 +11,13 @@ if (!empty($_POST['password']) && !empty($_POST['email'])) {
     $user = $statement->fetch();
 
     if ($user !== false && password_verify($password, $user['pswd'])) {
+        $_SESSION['user-log-status'] = $user['status'];
+        $_SESSION['user-id'] = $user['pk_id'];
+        //TODO redirect to Dashboard
         die('Login successful');
+    } else {
+        header("Location: ../login/login.html?err=1");
+        die;
     }
 
 }
