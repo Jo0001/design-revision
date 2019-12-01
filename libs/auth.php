20e441190c22bf7d1eceb1281b31206ab47e5615
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2019-12-01 12:32:30 +0100

    added auth lib and updated login

---
+++ libs/auth.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+
+function logIn($email, $password)
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=design_revision', 'dsnRev', '4_DiDsrev2019');
+    $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+    $result = $statement->execute(array('email' => $email));
+    $user = $statement->fetch();
+
+    if ($user !== false && password_verify($password, $user['pswd'])) {
+        $_SESSION['user-status'] = $user['status'];
+        $_SESSION['user-log'] = "true";
+        $_SESSION['user-id'] = $user['pk_id'];
+        $_SESSION['user-time'] = date("d-m-Y h:i:s");
+        header("Location: ../app/CustumorDashboard.html");
+        die;
+    } else {
+        header("Location: ../login/login.html?err=1");
+        die;
+    }
+
+}
+
+function isLoggedIn()
+{
+    if (isset($_SESSION['user-status']) && isset($_SESSION['user-id']) && isset($_SESSION['user-log']) && isset($_SESSION['user-time']) && $_SESSION['user-log'] == "true") {
+        //TODO Needs testing
+        $date1 = strtotime($_SESSION["auth-date"]);
+        $date2 = strtotime(date("d-m-Y h:i:s"));
+        $diff = abs($date2 - $date1) / 60 / 60 / 24;
+        if ($diff >= 1) {
+            logout();
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+function logOut()
+{
+    session_unset();
+    session_destroy();
+    header("Location: ../login/login.html?logout=true");
+    die;
+}
--- login/login.php
+++ login/login.php
@@ -1,23 +1,16 @@
 <?php
-session_start();
-$pdo = new PDO('mysql:host=localhost;dbname=design_revision', 'dsnRev', '4_DiDsrev2019');
+require "../libs/auth.php";
+if (isset($_GET['logout'])) {
+    logOut();
+    die;
+}
+if(isLoggedIn()){
+    header("Location: ../app/CustumorDashboard.html");
+    die;
+}
 //TODO check if user is logged in
 if (!empty($_POST['password']) && !empty($_POST['email'])) {
     $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
     $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
-
-    $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
-    $result = $statement->execute(array('email' => $email));
-    $user = $statement->fetch();
-
-    if ($user !== false && password_verify($password, $user['pswd'])) {
-        $_SESSION['user-log-status'] = $user['status'];
-        $_SESSION['user-id'] = $user['pk_id'];
-        //TODO redirect to Dashboard
-        die('Login successful');
-    } else {
-        header("Location: ../login/login.html?err=1");
-        die;
-    }
-
+    logIn($email, $password);
 }
