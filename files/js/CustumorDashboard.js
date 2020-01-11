579db03a87d82bb4d86e32f2878404546f03bad8
xXFiEsTaDeAmOnXx <niklas.kleiser@web.de>
2020-01-11 23:02:24 +0100

    added Member selcet option for project creation and added file upload to drag and drop plus updated design

--- files/js/CustumorDashboard.js
+++ files/js/CustumorDashboard.js
@@ -2,10 +2,10 @@ let a = true;
 let counter = 0;
 let customerid;
 let bool1 = false;
+let select = true;
+let sendArray=[];
+let sendFile;
 
-function emailIsValid(email) {
-    return (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))
-}
 
 function generate() {
     //Variablen erstellen
@@ -37,6 +37,7 @@ function generate() {
     customerdiv.appendChild(nameimg);
     //statusImg genereiren
     statusImg.setAttribute("alt", "tick");
+    statusImg.style.zIndex = "2";
     customerdiv.appendChild(statusImg);
     //Projektname generieren
     requestURL = "http://localhost/design-revision/api/?getproject&id=" + projectid;
@@ -131,7 +132,8 @@ function generate() {
     statusDiv.appendChild(textStatus);
     //Abfrage für den Status
     let boolStatus;
-    if (textStatus.innerHTML=== "Fertig/Druckfreigabe") {
+    //zum test != sonst ===
+    if (textStatus.innerHTML != "Fertig/Druckfreigabe") {
         statusImg.setAttribute("src", "../files/img/XBereit.png");
         boolStatus = true;
     } else {
@@ -141,29 +143,44 @@ function generate() {
     //Abfrage ob der Kunden gelöscht werden kann
 
     customerdiv.onclick = function () {
-        let id1 = clientname.innerHTML + projektname.innerHTML;
-        customerdiv.setAttribute("id", id1);
-        clientDivClick(clientname.innerHTML, projektname.innerHTML, id1, boolStatus, arrayMember, arrayRole);
-
+        if (select) {
+            let id1 = clientname.innerHTML + projektname.innerHTML;
+            customerdiv.setAttribute("id", id1);
+            clientDivClick(clientname.innerHTML, projektname.innerHTML, id1, boolStatus, arrayMember, arrayRole);
+        }
     };
 }
 
-function customerDelate() {
+function customerDelate(members, content, arrayLength) {
     let dialog = document.getElementById("dialog");
     dialog.setAttribute('open', 'open');
+    let btnYes = document.getElementById("btnYes");
+    btnYes.onclick = function () {
+        closeYes(members, content, arrayLength);
+    };
     toggleDialog();
 
 
 }
 
 //Button Ja gedrückt
-function closeYes() {
+function closeYes(members, content, arrayLength) {
     document.getElementById("dialog").removeAttribute('open');
     a = true;
     let divForm = document.getElementById("form1");
     let loeschen = document.getElementById("p1");
     divForm.removeChild(loeschen);
     let div = document.getElementById(customerid);
+    let id = div.getAttribute("data-id");
+    sendDelet(id);
+    //Daten der Cusomer löschen
+    for (let i = 0; i < arrayLength; i++) {
+        let help = content[i].getAttribute("data-id");
+        if (members.includes(help)) {
+            content[i].style.background = "white";
+            content[i].lastChild.remove();
+        }
+    }
     div.remove();
     toggleDialog();
 }
@@ -208,7 +225,9 @@ function clientDivClick(name1, projekt1, id1, boolStatus, members, role) {
         //Abfrage ob der Kunde gelöscht werden kann
         if (boolStatus) {
             loeschen.innerHTML = "Kunde löschen";
-            loeschen.setAttribute("onclick", "customerDelate()");
+            loeschen.onclick = function () {
+                customerDelate(members, content, arrayLength);
+            };
             loeschen.setAttribute("id", "p1");
             divForm.appendChild(loeschen);
             customerdiv1.style.border = "4px solid red";
@@ -252,7 +271,6 @@ function toggleDialog() {
         document.body.appendChild(div);
     } else {
         let div = document.querySelector('#backdrop');
-        div.parentNode.removeChild(div);
 
     }
 }
@@ -292,6 +310,7 @@ function showRes() {
     }
 }
 
+
 //Wartet bis die Seite geladen ist
 let readyStateCheckInterval = setInterval(function () {
     if (document.readyState === "complete") {
@@ -300,17 +319,15 @@ let readyStateCheckInterval = setInterval(function () {
         for (let i = 0; i <= 10; i++) {
             generate();
         }
-        //E-Mail validierung
-        let dashMail = document.getElementById("CustumorDashForm");
-        dashMail.addEventListener('submit', function (evt) {
-            if (!(emailIsValid(document.querySelector("#email").value))) {
-                window.alert("Email ist falsch ");
-                evt.preventDefault();
+
+        let CustumorDashForm  = document.getElementById("CustumorDashForm");
+        CustumorDashForm.addEventListener('submit', function (evt) {
+            if (sendFile===undefined||sendArray[0]===undefined){
+
             } else {
-
-                window.alert("Email richtig");
+                sendNewProject();
             }
-
+            evt.preventDefault();
         });
         //beim submit Event wird der Submit gehindert
         document.getElementById('search1').onsubmit = function (evt) {
@@ -319,7 +336,221 @@ let readyStateCheckInterval = setInterval(function () {
         // führt die Methode showRes beim Keyup_Event aus
         document.getElementById("searchform").addEventListener('keyup', showRes);
     }
+
+    //Datei Upload
+    const inputFile = document.getElementById("inputFile");
+    const previewContainer = document.getElementById("imagePreview");
+    const previewFile = previewContainer.querySelector(".image-preview__file");
+    const pdfIcon = document.getElementById("pdfIcon");
+    previewFile.onclick = function () {
+        pdfIcon.style.display = "none";
+        previewDefaulText.style.display = "block";
+        previewFile.style.display = "none";
+    };
+    pdfIcon.onclick = function () {
+        pdfIcon.style.display = "none";
+        previewDefaulText.style.display = "block";
+        previewFile.style.display = "none";
+    };
+    const previewDefaulText = previewContainer.querySelector(".image-preview__default-text");
+    previewContainer.addEventListener('dragover', handleDragOver, false);
+    previewContainer.addEventListener('drop', dateiauswahl, false);
+
+    inputFile.addEventListener("change", function () {
+        const file = this.files[0];
+        sendFile=file;
+        console.log(file);
+        if (file) {
+            pdfIcon.style.display = "block";
+            previewDefaulText.style.display = "none";
+            previewFile.style.display = "block";
+            previewFile.style.color = "black";
+            previewFile.style.fontSize = "12px";
+            previewFile.innerHTML = file.name + " (" + file.type + ")- " + file.size + " bytes,zuletzt Bearbeitet " + file.lastModifiedDate;
+        } else {
+            pdfIcon.style.display = "none";
+            previewDefaulText.style.display = "block";
+            previewFile.style.display = "none";
+        }
+    });
 }, 10);
 
+//Drag and Drop
+function dateiauswahl(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    let file = evt.dataTransfer.files;
+    let f = file[0];
+    sendFile=f;
+    let output = f.name + " (" + f.type + ")- " + f.size + " bytes,zuletzt Bearbeitet " + f.lastModifiedDate;
+    const previewContainer = document.getElementById("imagePreview");
+    const previewFile = previewContainer.querySelector(".image-preview__file");
+    const previewDefaulText = previewContainer.querySelector(".image-preview__default-text");
+    const pdfIcon = document.getElementById("pdfIcon");
+    pdfIcon.style.display = "block";
+    previewFile.innerHTML = output;
+    previewDefaulText.style.display = "none";
+    previewFile.style.display = "block";
+    previewFile.style.color = "black";
+    previewFile.style.fontSize = "12px";
+}
 
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy';
+}
 
+function addMember() {
+    let content = document.querySelectorAll('[data-id');
+    let arrayLength = content.length;
+    let addButton = document.getElementById("btnAddMember");
+    let jasonmembers = [];
+
+    if (select) {
+        if (a === false) {
+            //löschen nachricht verstecken
+            let delet = document.getElementById("form1");
+            //messageMember verstecken
+            delet.lastChild.style.display = "none";
+            for (let i = 0; i < arrayLength; i++) {
+                let temp = content[i].lastChild;
+                if (temp.innerHTML === "Ist Admin in dem Gewählten project" || temp.innerHTML === "Ist Mitglied in dem Gewählten project") {
+                    temp.style.display = "none";
+                }
+            }
+            a = true;
+        }
+        addButton.value = "Zu Projekt hinzufügen";
+        for (let i = 0; i < arrayLength; i++) {
+            content[i].style.background = "white";
+            content[i].style.border = "4px solid black";
+            let buttonAdmin = document.createElement("button");
+            buttonAdmin.innerHTML = "Admin";
+            //Butto Click event
+            buttonAdmin.addEventListener('click', function () {
+                let parent = buttonAdmin.parentNode;
+                let id = parent.getAttribute("data-id");
+                let role = 1;
+                let include = true;
+                //Schauen ob es den Member schon gibt un Rolle anpassen
+                for (let j = 0; j < jasonmembers.length; j++) {
+                    if (jasonmembers[i]) {
+                        if (jasonmembers[i].id === id) {
+                            jasonmembers[i].role = 1;
+                            include = false;
+                        }
+                    }
+                }
+                if (include) {
+                    let member = {"id": id, "role": role};
+                    jasonmembers.push(member);
+                }
+                jasonmembers.sort();
+                console.log(jasonmembers);
+                sendArray=jasonmembers;
+                parent.style.background = "#FFA500";
+            });
+            content[i].appendChild(buttonAdmin);
+            let buttonMember = document.createElement("button");
+            //Butto Click event
+            buttonMember.addEventListener('click', function () {
+                let parent = buttonMember.parentNode;
+                let id = parent.getAttribute("data-id");
+                let role = 0;
+                let include = true;
+                //Schauen ob es den Member schon gibt un Rolle anpassen
+                for (let j = 0; j < jasonmembers.length; j++) {
+                    if (jasonmembers[i]) {
+                        if (jasonmembers[i].id === id) {
+                            jasonmembers[i].role = 0;
+                            include = false;
+                        }
+                    }
+                }
+
+                if (include) {
+                    let member = {"id": id, "role": role};
+                    jasonmembers.push(member);
+                }
+                jasonmembers.sort();
+                console.log(jasonmembers);
+                sendArray=jasonmembers;
+                parent.style.background = "#00FF66"
+            });
+            buttonMember.innerHTML = "Member";
+            content[i].appendChild(buttonMember);
+            let buttonDeletMember = document.createElement("button");
+            buttonDeletMember.innerHTML = "Entfehrnen";
+            buttonDeletMember.addEventListener('click', function () {
+                let parent = buttonDeletMember.parentNode;
+                let id = parent.getAttribute("data-id");
+                for (let k in jasonmembers) {
+                    if (jasonmembers.hasOwnProperty(k)) {
+                        if (jasonmembers[k].id == id) {
+                            jasonmembers.splice(k, 1);
+                        }
+                    }
+
+                }
+                jasonmembers.sort();
+                console.log(jasonmembers);
+                sendArray=jasonmembers;
+                parent.style.background = "white";
+            });
+            content[i].appendChild(buttonDeletMember);
+            select = false;
+        }
+    } else {
+
+       console.log(sendArray);
+        addButton.value = "Member auswählen";
+        select = true;
+        for (let i = 0; i < arrayLength; i++) {
+            content[i].style.background = "white";
+            content[i].lastChild.remove();
+            content[i].lastChild.remove();
+            content[i].lastChild.remove();
+        }
+    }
+
+}
+function sendNewProject() {
+    let data = new FormData();
+    let projectname =document.getElementById("projectname").value;
+    data.append("createproject", "");
+    data.append("name", projectname);
+    data.append("members", sendArray);
+    data.append("file", sendFile);
+
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function() {
+        if(this.readyState === 4) {
+            console.log(this.responseText);
+        }
+    });
+    xhr.open("POST", "http://localhost/design-revision/api/");
+    xhr.send(data);
+
+}
+
+function sendDelet(id) {
+    let data = new FormData();
+    data.append("id", id);
+
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function() {
+        if(this.readyState === 4) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("DELETE", "http://localhost/design-revision/api/");
+
+    xhr.send(data);
+
+}
