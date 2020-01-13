00d0eaac5e9f94a02681ce5f7a4bc02767b17dbe
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2020-01-13 20:25:33 +0100

    changed links in emails and improved api

--- login/signup.php
+++ login/signup.php
@@ -32,7 +32,7 @@ if (!empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['
             $statement = $pdo->prepare("INSERT INTO users (name, company, email, pswd,status,token) VALUES (?,?,?,?,?,?)");
             $result = $statement->execute(array($name, $company, $email, $pswd, "registered", $hash));
             $user = $statement->fetch();
-            $link = "http://localhost/design-revision/app/verify.php?token=" . $hash;
+            $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://".filter_var($_SERVER['HTTP_HOST'], FILTER_SANITIZE_STRING)."/design-revision/app/verify.php?token=" . $hash;
             $content = parseHTML("../libs/templates/emailverify.html", $name, $link, null, null);
             sendMail($email, $name, "Willkommen bei Design Revison", $content);
 
