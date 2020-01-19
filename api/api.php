23042ad638dd77cc350c867afb5d45905398b3b3
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2020-01-19 14:03:46 +0100

    createproject is ready, changed email provider

--- api/api.php
+++ api/api.php
@@ -1,6 +1,7 @@
 <?php
 require "../libs/auth.php";
-//header("Access-Control-Allow-Origin: http://design-revision.ddns.net");
+require "../libs/sendEmail.php";
+
 $method = filter_var($_SERVER['REQUEST_METHOD'], FILTER_SANITIZE_STRING);
 
 function check_id()
@@ -69,7 +70,7 @@ function showError($error, $code)
         header("HTTP/1.1 500 Internal Server Error");
         $http_message = "Internal Server Error";
     }
-    $err = array("error" => array("message" => $error, "http-code" => $code, "http-message" => $http_message, "method" => $_SERVER['REQUEST_METHOD'], "query-string" => $_SERVER['QUERY_STRING'], "api-version" => 1.6));
+    $err = array("error" => array("message" => $error, "http-code" => $code, "http-message" => $http_message, "method" => $_SERVER['REQUEST_METHOD'], "query-string" => $_SERVER['QUERY_STRING'], "api-version" => 1.7));
     handleOutput($err);
     die;
 }
@@ -160,13 +161,70 @@ function createProject()
                 date_default_timezone_set('Europe/Berlin');
                 $date = date("Y-m-d H:i:s");
 
-                //TODO Check member data and inform sendMail to new members
                 $statement = $pdo->prepare("SELECT email FROM `users` ");
                 $statement->execute();
-                $useremails = $statement->fetchAll();
-                //demo data
+                $tmpmails = $statement->fetchAll();
+                //Create a one dimensional array with all email from the db
+                $useremails = array();
+                foreach ($tmpmails as $tmp) {
+                    array_push($useremails, $tmp['email']);
+                }
+                //convert json post to php array format
+                $members = json_decode($members, true);
+
                 $memberids = array();
 
+                $pid = explode("project_",$t_name)[1];
+
+                $projectname = $name;
+
+                //add current user (as admin)
+                $userid = getUser('pk_id');
+                array_push($memberids, array("id" => (int)$userid, "role" => 1));
+
+                foreach ($useremails as $tmp) {
+                    //check if user has an account
+                    if (in_array($tmp, array_column($members, 'email'))) {
+                        $id = emailToId($tmp);
+                        //get array with all roles and emails as index and select the one equal to the current email and save it
+                        $role = array_column($members, 'role', 'email')[$tmp];
+                        array_push($memberids, array("id" => (int)$id, "role" => $role));
+                        //fetch all projectdata of the user
+                        $statement = $pdo->prepare("SELECT projects FROM `users` WHERE pk_id = ? ");
+                        $statement->execute(array($id));
+                        $tmpprojects = json_decode($statement->fetch()[0]);
+
+                        //and save the new project to the user
+                        array_push($tmpprojects, $pid);
+                        $statement = $pdo->prepare("UPDATE `users` SET `projects` = ? WHERE `users`.`pk_id` = ?");
+                        $statement->execute(array(json_encode($tmpprojects), $id));
+
+                        $statement = $pdo->prepare("SELECT name FROM `users` WHERE pk_id = ?");
+                        $statement->execute(array($id));
+                        //Inform user per email about the new project
+                        $name = $statement->fetch()[0];
+                        $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . filter_var($_SERVER['HTTP_HOST'], FILTER_SANITIZE_STRING) . "/design-revision/simulate/edit.php?id=" . $pid;
+                        sendMail($tmp, $name, "Einladung zu \"" . $projectname . "\"", parseHTML("../libs/templates/emailFreigebenAcc.html", $name, $link, $projectname, 1));
+                    }
+                }
+                //save all email addresses of the members as one dimensional array
+                $emails = array();
+                foreach ($members as $tmp) {
+                    array_push($emails, $tmp['email']);
+                }
+
+                $result = array_diff($emails, $useremails);
+                if (!empty($result)) {
+                    foreach ($result as $tmp) {
+                        $statement = $pdo->prepare("INSERT INTO `users` (`pk_id`, `name`, `company`, `email`, `pswd`, `projects`, `status`, `token`, `token_timestamp`) VALUES (NULL, '', NULL, ?, '', ?, 'INVITE', NULL, CURRENT_TIMESTAMP)");
+                        $statement->execute(array($tmp, json_encode(array($pid))));
+                        $role = array_column($members, 'role', 'email')[$tmp];
+                        array_push($memberids, array("id" => (int)$pdo->lastInsertId(), "role" => $role));
+
+                        $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . filter_var($_SERVER['HTTP_HOST'], FILTER_SANITIZE_STRING) . "/design-revision/login/loginNewAccount.html?email=" . $tmp;
+                        sendMail($tmp, $tmp, "Einladung zu \"" . $projectname . "\"", parseHTML("../libs/templates/emailFreigebenNew.html", null, $link, $projectname, 1));
+                    }
+                }
 
                 $statement = $pdo->prepare("INSERT INTO " . $t_name . " (p_name, link, members,status,lastedit) VALUES (?, ?, ?,?,?)");
                 $statement->execute(array($name, $filename, json_encode($memberids), 'WAITING_FOR_RESPONSE', $date));
@@ -226,7 +284,7 @@ function deleteProject()
     parse_str(file_get_contents('php://input'), $_DELETE);
     if (isset($_DELETE['id'])) {
         //TODO Needs testing
-        $id = "project_".filter_var($_DELETE['id'], FILTER_SANITIZE_STRING);
+        $id = "project_" . filter_var($_DELETE['id'], FILTER_SANITIZE_STRING);
 
         $pdo = new PDO('mysql:host=localhost;dbname=design_revision', 'dsnRev', '4_DiDsrev2019');
         if (isValidProject($id, $pdo) && getUser('status') == "VERIFIED") {
@@ -295,7 +353,8 @@ function isAdmin($lastrow, $userid)
     }
 }
 
-function emailToId($email){
+function emailToId($email)
+{
     $pdo = new PDO('mysql:host=localhost;dbname=design_revision', 'dsnRev', '4_DiDsrev2019');
     $statement = $pdo->prepare("SELECT pk_id FROM `users` WHERE email = ?");
     $statement->execute(array($email));
