13ac16ac7b72514efa1e44cd575403403dd566c7
David <david-metzler@web.de>
2020-01-27 17:26:00 +0100

    PDF-Viewer bugfix for older FF-Browsers

--- files/js/viewport.js
+++ files/js/viewport.js
@@ -5,8 +5,6 @@ let pdfjsLib = window['pdfjs-dist/build/pdf'];
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.3.200/pdf.worker.js';
 
 class TargetScaleHandlerClass {
-    timeSinceResize;
-
     constructor() {
         this.isScalingATM = false;
         this.innerTargetScale = 1;
