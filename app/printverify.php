e2157e1e83b6e132233bb0f8fd7985d6931f3a6c
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2020-03-02 16:00:59 +0100

    finished printrelease

--- app/printverify.php
+++ app/printverify.php
@@ -1,7 +1,60 @@
 <?php
+require "../libs/auth.php";
+require "../libs/api-util.php";
+require "../libs/sendEmail.php";
+
 if (!empty($_POST['code']) && !empty($_GET['id'])) {
-    $code =   filter_var($_POST ['code'], FILTER_SANITIZE_STRING);
-    $pid =   filter_var($_GET ['id'], FILTER_SANITIZE_STRING);
+    $code = filter_var($_POST ['code'], FILTER_SANITIZE_STRING);
+    $pid = filter_var($_GET ['id'], FILTER_SANITIZE_STRING);
+    if (isLoggedIn()) {
+        $pid = "project_" . $pid;
+        $loc = "?id=" . explode("project_", $pid)[1];
+        if (isValidProject($pid, $pdo)) {
+            if (isMember($pid, getUser('pk_id'))) {
+                $pdo = new PDO('mysql:host=localhost;dbname=design_revision', 'dsnRev', '4_DiDsrev2019');
+                $project = getLatestProjectData($pid, $pdo);
+                $securitycode = $project['securitycode'];
+
+                if ($project['status'] !== DONE) {
+                    if ($code === $securitycode) {
+
+                        $statement = $pdo->prepare("UPDATE " . $pid . " SET `securitycode` = ?  ORDER BY version DESC LIMIT 1");
+                        $statement->execute(array(null));
+
+                        //Sendmail to all members
+                        $uname = getUser('name');
+                        $pname = $project['p_name'];
+                        $link = $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . filter_var($_SERVER['HTTP_HOST'], FILTER_SANITIZE_STRING) . "/design-revision/simulate/view.php?id=" . explode("project_", $pid)[1];
+
+                        $members = json_decode($project['members'], true);
+                        foreach ($members as $member) {
+                            $id = $member['id'];
+                            $email = IdToEmail($pdo, $id);
+                            $toName = IdToName($pdo, $id);
+                            sendMail($email, $toName, "'" . $pname . "' wurde erfolgreich zum Druck freigegeben", parseHTML("../libs/templates/emailDruckFreigegeben.html", $uname, $link, $pname, null));
+                        }
+
+                        changeStatus($pdo, $pid, DONE);
+
+                        header("Location: ?done=1");
+
+                    } else {
+                        header("Location: $loc&err=code");
+                    }
+                } else {
+                    header("Location: $loc&err=status");
+                }
+
+            } else {
+                header("Location: $loc&err=member");
+            }
+        } else {
+            header("Location: $loc&err=project");
+        }
+    } else {
+        header("Location: ../login/?returnto=" . getCurrentURL());
+        die;
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -23,4 +76,10 @@ if (!empty($_POST['code']) && !empty($_GET['id'])) {
 </div>
 </body>
 </html>
-
+<!--
+TODO Handle success & error @Frontend
+err=status ->Project is already done
+err=code ->Invalid/Wrong Code
+err=member ->Not a project member
+err=project->invalid project
+-->
