644c275d98d7acdf5bcf586135a9e883d411713a
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2020-04-04 13:22:23 +0200

    added config file

---
+++ libs/config.php
@@ -0,0 +1,13 @@
+<?php
+/*Email Settings*/
+define("emailHost","smtp.sendgrid.net");
+define("emailUsername","apikey");
+define("emailPassword","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");
+define("emailPort",587);
+define("emailFromaddress","design-revision@gmx.de");
+define("emailFromname","Design Revision");
+
+/*Database Settings*/
+define("dbDsn","mysql:host=localhost;dbname=design_revision");
+define("dbUsername","dsnRev");
+define("dbPassword","4_DiDsrev2019");
