2362a693e86d9f506f92e699e3fbb0710b920738
David <david-metzler@web.de>
2020-04-27 19:16:14 +0200

    fixed Jonas' filter.php

--- libs/filter.php
+++ libs/filter.php
@@ -47,8 +47,7 @@ function filterComment($comment)
   Max type value: 9
   Max version value: 99
   */
-            //return ($comment['page'] <= 999 && isValidPos($comment['x']) && isValidPos($comment['y']) && isValidPos($comment['w']) && isValidPos($comment['h']) && strlen($comment['authorId']) <= 4 && strlen($comment['commentText']) <= 250 && is_bool($comment['isImplemented']) && strlen($comment['color']) == 7 && strlen($comment['cid']) <= 10 && strlen($comment['type']) == 1 && strlen($comment['version']) <= 99);
-        return true;
+            return ($comment['page'] <= 999 && strlen($comment['x']) == 10 && strlen($comment['y']) == 10 && isValidPos($comment['w']) && isValidPos($comment['h']) && strlen($comment['authorId']) <= 4 && strlen($comment['commentText']) <= 250 && is_bool($comment['isImplemented']) && strlen($comment['color']) == 7  && strlen($comment['cid']) <= 10 && strlen($comment['type']) == 1 && strlen($comment['version']) <= 99);
         }
         return false;
     } else {
